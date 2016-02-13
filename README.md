9c7657991b9b02a52db6d16f1a8e615dc76e3185
Alan R <alan.rominger@gmail.com>
2016-02-12 20:18:24 -0500

    fields gobbled up and exception free for points in first file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gpx-analyizer
 Project to gobble up some personal Garmin navigator data
+
+## Instructions
+
+Plug in your Garmin unit to your computer and copy over the files in the
+gpx archive folder into the archive folder of this project. Then run the
+analyizer.
+
+## Questions
+
+How can we take our extremely obvious intuition and apply it to a large set
+of data like this? Some boundaries are easy to draw. I'm pretty sure my car
+never went 1,000 miles per hour, so if we see that, it's safe to say that we
+can mark some points as invalid. But our intuition is much more detailed than
+this.
--- analyize.py
+++ analyize.py
@@ -2,9 +2,45 @@ import os
 import re
 
 class Point(object):
+	full_string = None
+	time = None
+	speed = None
+	course = None
+	elevation = None
+	lat = None
+	lon = None
 
-	def extract_field(name):
-		p = '(?P<trkpt>\<trkpt.*?\/trkpt\>)'
+	def __init__(self, stx):
+		self.full_string = stx
+
+	def store_fields(self):
+		self.time = pt.extract_field('time')
+		self.speed = pt.extract_field('speed')
+		self.course = pt.extract_field('course')
+		self.elevation = pt.extract_field('ele')
+		(self.lat, self.lon) = pt.extract_cords()
+
+	def extract_field(self, name):
+		prefix = ''
+		if name in ('TrackPointExtension', 'speed', 'course'):
+			prefix = 'gpxtpx:'
+		pattern = '\<' + prefix + name + '\>(?P<guts>.*?)\<\/' + prefix + name + '\>'
+		finds = re.findall(pattern, self.full_string)
+		if len(finds) != 1:
+			if name == 'time' or name == 'ele':
+				raise Exception(str(len(finds)) + ' duplicate fields found for '+name)
+			else:
+				return None
+		return finds[0]
+
+	def extract_cords(self):
+		pattern = re.compile('\<trkpt\slat="(?P<lat>-?[0-9]+\.[0-9]+)"\slon="(?P<lon>-?[0-9]+\.[0-9]+)"\>')
+		match = pattern.match(self.full_string)
+		lat = match.group('lat')
+		lon = match.group('lon')
+		if lat is None or lon is None:
+			raise Exception('Lattitude and longitude for point not found')
+		return (lat, lon)
 
 
 # Put your gpx archive files in the archive directory
@@ -15,12 +51,12 @@ filelist = os.listdir(archive_dir)
 # Sort file list numerically, not alphabetically
 filelist = sorted(filelist, key=lambda x: float(x[:-4]))
 
-
-print(filelist)
+print('\nFiles found in archive folder:')
+print(' '.join(filelist) + '\n')
 
 filename = filelist[0]
 
-with open(os.path.join(archive_dir, filename)) as f:
+with open(os.path.join(archive_dir, filename), 'r') as f:
 	full_file = f.read()
 
 pattern = '(?P<trkpt>\<trkpt.*?\/trkpt\>)'
@@ -32,4 +68,24 @@ print("")
 
 for i in range(3):
 	print(points[i])
-	print("")
+	pt = Point(points[i])
+	print("time: "+pt.extract_field('time'))
+	print("speed: "+pt.extract_field('speed'))
+	print("course: "+pt.extract_field('course'))
+	print('cords: ' + str(pt.extract_cords()))
+	pt.store_fields()
+	print('')
+
+print('Testing entirity of the points:')
+i = 0
+for ptxt in points:
+	i += 1
+	pt = Point(ptxt)
+	try:
+		pt.store_fields()
+	except Exception as ex:
+		print('')
+		print('Failed on point # ' + str(i))
+		print('time: ' + str(pt.time) + ' elevation: ' + str(pt.elevation) + ' course: ' + str(pt.course))
+		print('Text: ' + ptxt)
+		print(ex)
