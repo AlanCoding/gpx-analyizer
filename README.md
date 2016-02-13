415b9f24d56810bd2a5c2da396f0964a40533623
Alan R <alan.rominger@gmail.com>
2016-02-12 22:46:58 -0500

    histogram stuff

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Project to gobble up some personal Garmin navigator data
 
 Plug in your Garmin unit to your computer and copy over the files in the
 gpx archive folder into the archive folder of this project. Then run the
-analyizer.
+analyzer.
 
 ## Questions
 
@@ -14,3 +14,66 @@ of data like this? Some boundaries are easy to draw. I'm pretty sure my car
 never went 1,000 miles per hour, so if we see that, it's safe to say that we
 can mark some points as invalid. But our intuition is much more detailed than
 this.
+
+Here are some details about the format in general:
+
+http://www.topografix.com/gpx_manual.asp
+
+## Discoveries
+
+## Packaged speed values
+
+The Garmin units appear to record speed only in specific increments. These
+increments seem to be 1.37 or 1.38, depending on how the rounding works out.
+Here are a few of the first set of speeds that the unit uses:
+
+    1.37, 2.75, 4.12, 5.49, 6.86, 8.24, 9.61, 10.98, 12.35, 13.73, 15.1, 16.47
+
+In terms of units, there are almost certainly in terms of meters per second.
+
+## Example outputs
+
+Here is my own speed histogram, using bins defined by the native storage
+method of speeds in the Garmin unit. These are converted to mph units below.
+
+		upper_bound      frequency
+		3.0646  ######################
+		6.1516  ################################
+		9.2162  ###############################################
+		12.2808 ###########################################################################
+		15.3454 ###################################################################
+		18.4324 ####################################################
+		21.497  ##########################################
+		24.5616 ####################################
+		27.6262 ##################################
+		30.7132 #####################################
+		33.7778 ####################################
+		36.8424 #######################################
+		39.907  ######################################
+		42.994  ##################################
+		46.0586 ###########################
+		49.1232 #######################
+		52.1878 #####################
+		55.2748 #####################
+		58.3394 ######################
+		61.404  ######################
+		64.4686 ###########################
+		67.5556 #########################
+		70.6202 #################
+		73.6848 ########
+		76.7494 ##
+		79.8364
+		82.901
+		95.1818
+		101.311
+		104.398
+		122.808
+		150.4342
+
+Why does the upper scale go up to 150 mph? Because it had data for that speed.
+No, I assure you I never drove this fast. It's a numerical error that sometimes
+pops up. This is out of >250,000 speed points, so it's not unreasonable that
+it produced a dozen outliers due to equipment malfunction. I'm interested
+to find out more about this small number of points. It seems likely that
+those points were closer to the detection limits, so we would expect service
+cutoff or other sporadic behavior around those points.
---
+++ analyze.py
@@ -0,0 +1,145 @@
+import os
+import re
+import sys
+
+class Point(object):
+	full_string = None
+	time = None
+	speed = None
+	course = None
+	elevation = None
+	lat = None
+	lon = None
+
+	def __init__(self, stx):
+		self.full_string = stx
+
+	def store_fields(self):
+		self.time = pt.extract_field('time')
+		speed = pt.extract_field('speed')
+		self.speed = float(speed) if type(speed) is str else None
+		course = pt.extract_field('course')
+		self.course = float(course) if type(course) is str else None
+		self.elevation = float(pt.extract_field('ele'))
+		(self.lat, self.lon) = pt.extract_cords()
+
+	def extract_field(self, name):
+		prefix = ''
+		if name in ('TrackPointExtension', 'speed', 'course'):
+			prefix = 'gpxtpx:'
+		pattern = '\<' + prefix + name + '\>(?P<guts>.*?)\<\/' + prefix + name + '\>'
+		finds = re.findall(pattern, self.full_string)
+		if len(finds) != 1:
+			if name == 'time' or name == 'ele':
+				raise Exception(str(len(finds)) + ' duplicate fields found for '+name)
+			else:
+				return None
+		return finds[0]
+
+	def extract_cords(self):
+		pattern = re.compile('\<trkpt\slat="(?P<lat>-?[0-9]+\.[0-9]+)"\slon="(?P<lon>-?[0-9]+\.[0-9]+)"\>')
+		match = pattern.match(self.full_string)
+		lat = match.group('lat')
+		lon = match.group('lon')
+		if lat is None or lon is None:
+			raise Exception('Lattitude and longitude for point not found')
+		return (lat, lon)
+
+
+# Put your gpx archive files in the archive directory
+cwd = os.getcwd()
+archive_dir = os.path.join(cwd, 'archive/')
+filelist = os.listdir(archive_dir)
+
+# Sort file list numerically, not alphabetically
+filelist = sorted(filelist, key=lambda x: float(x[:-4]))
+
+print('\nFiles found in archive folder:')
+print(' '.join(filelist) + '\n')
+
+filename = filelist[0]
+
+with open(os.path.join(archive_dir, filename), 'r') as f:
+	full_file = f.read()
+
+pattern = '(?P<trkpt>\<trkpt.*?\/trkpt\>)'
+
+points = re.findall(pattern, full_file)
+
+print(' Number of points in set: '+str(len(points)))
+print('')
+
+print('Example:')
+for i in range(1):
+	print(points[i])
+	pt = Point(points[i])
+	print("time: "+pt.extract_field('time'))
+	print("speed: "+pt.extract_field('speed'))
+	print("course: "+pt.extract_field('course'))
+	print('cords: ' + str(pt.extract_cords()))
+	pt.store_fields()
+	print('speed times 5 ' + str(pt.speed>1))
+	print('')
+
+# Cycle through entire data
+print('Testing entirity of the files:')
+
+hist_bins = 100
+hist_delta = 1
+hist_max_width = 75
+shist = [0 for i in range(hist_bins)]
+
+hist_dict = {}
+
+sys.stdout.write('filenames: ')
+sys.stdout.flush()
+for filename in filelist:
+	sys.stdout.write(' ' + filename)
+	sys.stdout.flush()
+	with open(os.path.join(archive_dir, filename), 'r') as f:
+		full_file = f.read()
+
+	points = re.findall(pattern, full_file)
+
+	i = 0
+	for ptxt in points:
+		i += 1
+		pt = Point(ptxt)
+		try:
+			pt.store_fields()
+		except Exception as ex:
+			print('')
+			print('Failed on point # ' + str(i))
+			print('time: ' + str(pt.time) + ' elevation: ' + str(pt.elevation) + ' course: ' + str(pt.course))
+			print('Text: ' + ptxt)
+			print(ex)
+
+		if i > 1:
+			if pt.speed is not None:
+				# if pt.speed > 50.:
+				# 	print('Super fast point' + str(pt.speed))
+				shist[int(pt.speed/hist_delta)] += 1
+				if pt.speed not in hist_dict:
+					hist_dict[pt.speed] = 1
+				else:
+					hist_dict[pt.speed] += 1
+
+		last = pt
+
+print('')
+print('Speed histogram:')
+print(' total sample points= ' + str(sum(shist)))
+# shist = [hist_max_width * s / max(shist) for s in shist]
+# for i in range(hist_bins):
+# 	print((str(hist_delta*(i-1)) + '-to-' + str(hist_delta*i)).ljust(9) + '#' * int(shist[i]))
+
+# print('')
+print(hist_dict)
+
+print('upper_bound      frequency')
+hist_max = max(hist_dict.values())
+for k in sorted(hist_dict.keys()):
+	print(str(round(k*2.23694,4)).ljust(8) + '#' * int(hist_dict[k] * hist_max_width / hist_max) )
+
+print('')
+print('sorted values: ' + str(sorted(hist_dict.keys())))
