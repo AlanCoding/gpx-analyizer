8e86625b0d9d98521ef255bc91cf0f145dab1c59
Alan R <alan.rominger@gmail.com>
2016-03-01 21:58:23 -0500

    started on code for arbitrary value histogram

--- gpxstats/processors.py
+++ gpxstats/processors.py
@@ -84,6 +84,73 @@ class PrintFirst100(object):
 		pass
 
 
+class AttributeHistogram(object):
+	fields = ['speed_calc', 'acceleration_calc', 'dist', 'elevation']
+	# Size of the histogram to chop it up by
+	deltas = dict(
+		'speed_calc': 1.,
+		'acceleration_calc': 0.01,
+		'dist': 5.,
+		'elevation': 10.
+	)
+	# Minimum values to accept
+	mins = dict(
+		'speed_calc': 0.,
+		'acceleration_calc': -0.5,
+		'dist': 0.,
+		'elevation': 0.
+	)
+	hist_array = None
+	overs = None
+	unders = None
+	# Number of points to store
+	hist_max_width = 75
+
+	def __init__(self):
+		self.hist_array = {}
+		self.unders = {}
+		self.overs = {}
+		for fd in self.fields:
+			self.hist_array[fd] = {}
+			self.overs[fd] = 0
+			self.unders[fd] = 0
+
+	def update(self, point):
+		for fd in self.fields:
+			the_val = getattr(point, fd)
+			if not type(the_val) is float:
+				continue
+			place = int((the_val - self.mins[fd])/self.deltas[fd])
+			if place < 0:
+				self.unders += 1
+				continue
+			if place > self.hist_max_width:
+				self.overs += 1
+				continue
+			if place in self.hist_array[fd]:
+				self.hist_array[fd][place] = 1
+			else:
+				self.hist_array[fd][place] += 1
+
+	def display(self):
+
+		for fd in self.fields:
+			print('\n')
+			print('%s histogram:' % fd)
+			print(' total sample points= ' + str(sum(self.hist_array[fd])))
+
+			print('upper_bound      frequency')
+			hist_max = max(self.hist_dict.values())
+			for k in sorted(self.hist_dict.keys()):
+				print(str(round(k*2.23694,2)).ljust(7) +
+					'#' * int(self.hist_dict[k] * self.hist_max_width / hist_max) +
+					'   ' + str(self.hist_dict[k]))
+
+			print('')
+
+
+# TODO: Processor to store an array of stops and their statistics
+
 class SpeedHistogram(object):
 	hist_delta = 1
 	hist_max_width = 75
